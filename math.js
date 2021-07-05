b414a4d7be3fef9e9543386e15c68be3ae541660
oznerol <lorenzo.ghidoni92@gmail.com>
2021-07-05 17:37:43 +0100

    valueFromTo func

--- math.js
+++ math.js
@@ -1,7 +1,20 @@
-const roundNumber = function (number, decimals) {
+const roundNumber = (number, decimals) => {
     return Math.round((number)*Math.pow(10, decimals)) / Math.pow(10, decimals);
 }
 
+const valueFromTo = (value, from, to) => {
+    if (from != to) {
+        for (let i = 0; i < all_tickers.length; i++) {
+            if (to === 'USDT') {
+                let str = from + '_' + to;
+                let coin_usdt = all_tickers.filter(ticker => ticker.i === str)[0];
+                return value * coin_usdt.b;
+            };
+        };
+    };
+};
+
 module.exports = {
-    roundNumber
+    roundNumber,
+    valueFromTo
 }
